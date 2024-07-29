0427236c1835b83af3e44cf1520b3d091a284fdc
peletor <peletor@yandex.ru>
2024-07-29 19:38:37 +0300

    Add logger in main.go

--- cmd/url_shortener/main.go
+++ cmd/url_shortener/main.go
@@ -1,17 +1,53 @@
 package main
 
 import (
+	"log/slog"
+	"os"
 	"rest_api_shortener/internal/config"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	// TODO: init logger: slog
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("Debug messages enabled")
 
 	// TODO: init storage: sqlite
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server
+}
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envTest  = "test"
+	envProd  = "prod"
+)
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+	switch env {
+	case envLocal:
+		logger = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envTest:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelWarn}),
+		)
+	case envProd:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return logger
 }
