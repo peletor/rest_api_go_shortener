afb04d96ff310abe17682ff939bbeb48c5dabcad
Dmitry V <peletor@yandex.ru>
2024-07-29 17:30:35 +0300

    Add main.go file

---
+++ cmd/url_shortener/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// TODO: init config: cleanenv
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi, "chi render"
+
+	// TODO: run server
+}
